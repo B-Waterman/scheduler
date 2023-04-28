ec5108354e250e93a1e64102f460bba8d17a076e
B-Waterman <brooke.waterman@outlook.com>
2023-04-28 06:30:47 -0700

    Conform naming of dayItem to convention defined in scss styling, 'day-list__item'. Pass props.name & props.spots to className.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,17 +5,17 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  let dayItemClass = classNames("dayItem", {
-    "dayItem--selected": props.selected,
-    "dayItem--full": !props.spots
+  let dayItemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
   });
   return (
     <li
       className={dayItemClass}
       onClick={() => props.setDay(props.name)}
     >
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
-    </li>
-  );
-};
+    <h2 className="text--regular">{props.name}</h2>
+    <h3 className="text--light">{props.spots} spots remaining</h3>
+  </li>
+);
+}
