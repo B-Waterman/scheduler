599652b4a3c09fe31be748772653217a7b3e0516
B-Waterman <brooke.waterman@outlook.com>
2023-04-28 08:39:25 -0700

    Styling & syntax refactoring

--- src/components/Application.js
+++ src/components/Application.js
@@ -32,11 +32,11 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
-          />
+        <DayList
+          days={days}
+          day={"Monday"}
+          setDay={day => console.log(day)}
+        />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
