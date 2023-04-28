7fdfd1342d7820d44385800e89ca5c2438b72d33
B-Waterman <brooke.waterman@outlook.com>
2023-04-28 06:45:29 -0700

    Create formatSpots function to provide a dynamic message showing how many spots left for given DayListItem.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,7 +4,18 @@ import classNames from "classnames";
 
 import "components/DayListItem.scss";
 
+const formatSpots = function(spots) {
+  if(!spots) {
+    return "no spots remaining";
+  }
+  if(spots === 1) {
+    return "1 spot remaining";
+  }
+  return `${spots} spots remaining`;
+};
+
 export default function DayListItem(props) {
+  const spotsRemaining = formatSpots(props.spots);
   let dayItemClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
@@ -14,8 +25,8 @@ export default function DayListItem(props) {
       className={dayItemClass}
       onClick={() => props.setDay(props.name)}
     >
-    <h2 className="text--regular">{props.name}</h2>
-    <h3 className="text--light">{props.spots} spots remaining</h3>
-  </li>
-);
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{spotsRemaining}</h3>
+    </li>
+  );
 }
