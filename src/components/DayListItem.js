64e537433bf5a6612450780aab0ef8b0876aca94
B-Waterman <brooke.waterman@outlook.com>
2023-04-28 06:08:55 -0700

    Implemenet onClick event handler to set day of appointment. Import classNames library to define props.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import classNames from "classnames";
+
 export default function DayListItem(props) {
+  let dayItemClass = classNames("dayItem", {
+    "dayItem--selected": props.selected,
+    "dayItem--full": !props.spots
+  });
   return (
-    <li>
+    <li
+      className={dayItemClass}
+      onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">Day Name</h2> 
       <h3 className="text--light">X spots remaining</h3>
     </li>
   );
-}
+};
