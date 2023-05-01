8f67f0fa4526e6bbc3142802e315779585061abb
B-Waterman <brooke.waterman@outlook.com>
2023-05-01 05:40:29 -0700

    Import Header, Show, & Empty to index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,10 @@ import classNames from "classnames";
 
 import "components/Appointment/styles.scss";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 export default function Appointment(props) {
   return (
     <article className="appointment"></article>
