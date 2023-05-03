ce63cae179cb1c7275b97ee30cef1ccde3575268
B-Waterman <brooke.waterman@outlook.com>
2023-05-03 10:56:42 -0700

    Add CREATE-mode to show the Form, refactor EMPTY-mode to employ transition and CREATE onAdd.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,11 +7,13 @@ import useVisualMode from "hooks/useVisualMode";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 
 const SHOW = "SHOW";
 const EMPTY = "EMPTY";
+const CREATE = "CREATE";
 
-export default function Appointment({time, interview}) {
+export default function Appointment({time, interview, name, value}) {
 
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY);
@@ -19,13 +21,14 @@ export default function Appointment({time, interview}) {
   return (
     <article className="appointment">
       <Header time={time}/>
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          student={interview.student}
+          interviewer={interview.interviewer}
         />
       )}
+      {mode === CREATE && <Form interviewers={[]} name={name} value={value} onCancel={back} />}
     </article>
   );
 };
