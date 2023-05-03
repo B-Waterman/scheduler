2994434bec25bbae82a0d0ca0deeb69e5d2675e9
B-Waterman <brooke.waterman@outlook.com>
2023-05-03 12:13:51 -0700

    Add save function to Appointment to be passed to Form - WIP

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "components/Appointment/styles.scss";
 
@@ -8,15 +8,30 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 
 const SHOW = "SHOW";
 const EMPTY = "EMPTY";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
-export default function Appointment({time, interview, name, value}) {
+export default function Appointment({time, interview, id, name, value, bookInterview}) {
 
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY);
+    useEffect(() => {
+      if (interview && mode === EMPTY) {
+        transition
+      }
+    });
+
+  function save(name, interviewer) {
+    transition(SAVING);
+    const interview = {
+      student: name,
+      interviewer
+    };
+  }
 
   return (
     <article className="appointment">
