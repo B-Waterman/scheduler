2105cbde699c317c6bd2314a3dd343bb5c0617ee
B-Waterman <brooke.waterman@outlook.com>
2023-05-03 10:42:19 -0700

    Import useVisualMode to Appointment.index, add mode constants EMPTY & SHOW, conditionally render components - WIP.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,18 +2,30 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
+import useVisualMode from "hooks/useVisualMode";
+
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
+const SHOW = "SHOW";
+const EMPTY = "EMPTY";
+
 export default function Appointment({time, interview}) {
+
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={time}/>
-      {interview ?
-        <Show student={interview.student} interviewer={[interview.interviewer]} />
-        : <Empty/>
-      }
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 };
