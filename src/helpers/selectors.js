c94c386218ea97065e13a84f93c2269b9bdaae6c
B-Waterman <brooke.waterman@outlook.com>
2023-05-03 08:09:39 -0700

    Implement getInterview selector.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,19 @@
 export function getAppointmentsForDay(state, day) {
   const dayAppointments = [];
-  const dayAppointmentID = state.days.find((element) => element.name === day)?.appointments || undefined;
+  const dayAppointmentID = state.days.find((element) => element.name === day)?.appointments;
 
   dayAppointmentID && dayAppointmentID.forEach((id) => {
     dayAppointments.push(state.appointments[id])
   });
 
   return dayAppointments;
+};
+
+export function getInterview(state, interview) {
+  const interviewSlot = interview ? {...interview} : null
+  if (interviewSlot) {
+    interviewSlot.interviewer =
+  state.interviewers[interviewSlot.interviewer]
+  }
+  return
 };
