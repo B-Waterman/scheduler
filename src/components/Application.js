5134514a2be5ef51eea83b2816e501a65e76dff8
B-Waterman <brooke.waterman@outlook.com>
2023-05-03 11:16:31 -0700

    Fix axios request, https>http

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,11 +21,11 @@ export default function Application(props) {
 
   useEffect(() => {
     Promise.all([
-      axios.get('https://localhost:8001/api/days'),
-      axios.get('https://localhost:8001/api/appointments'),
-      axios.get('https://localhost:8001/api/interviewers')
-    ]).then((resDays, resAppts, resInt) => {
-      setState(prev => ({ ...prev, days: resDays.data, appointments: resAppts.data, interviewers: resInt.data }));
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
   }, []);
 
