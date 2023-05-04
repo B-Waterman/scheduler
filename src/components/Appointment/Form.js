dcd79d0636a9dfef23183fcc22c2ea1a03a4e748
B-Waterman <brooke.waterman@outlook.com>
2023-05-04 06:44:32 -0700

    Add error handling for appointment creation(student name input). Current bug: cannot read properties of null: student. WIP

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,9 +5,10 @@ import Button from "components/Button";
 
 
 //A form to track the page's text input value & selected interviewer
-export default function Form({interviewers, name, onCancel, onSave, ...rest}) {
+export default function Form({interviewers, onCancel, onSave, ...rest}) {
   const [student, setStudent] = useState(rest.student || "");
   const [interviewer, setInterviewer] = useState(rest.interviewer || null);
+  const [error, setError] = useState("");
 
   //Helper Functions
   const reset = function() {
@@ -20,19 +21,33 @@ export default function Form({interviewers, name, onCancel, onSave, ...rest}) {
     onCancel()
   };
 
+  const checkContent = function(){
+    if (!student || student === "" ) {
+      setError("Please enter a student name");
+      return;
+    }
+    if (!interviewer) {
+      setError("Please select an interviewer");
+      return;
+    }
+    setError("");
+    onSave(student, interviewer);
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={name}
+            name="name"
             type="text"
             onChange={(event) => setStudent(event.target.value)}
             placeholder="Enter Student Name"
-            student={student}
+            value={student}
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={interviewers}
           interviewer={interviewer}
@@ -42,7 +57,7 @@ export default function Form({interviewers, name, onCancel, onSave, ...rest}) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={checkContent}>Save</Button>
         </section>
       </section>
     </main>
