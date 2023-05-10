69f97044650f33945afed4bda5b4a6d8b9e077a9
B-Waterman <brooke.waterman@outlook.com>
2023-05-10 12:56:28 -0700

    Refactor placement of useEffect, remove unecessary 'res', & remove hard-coded URLs.

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -11,6 +11,15 @@ export default function useApplicationData() {
   });
 
   const setDay = day => setState({ ...state, day });
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      })
+  }, []);
 
   function updateSpots(state, id, appointments) {
     const days = [...state.days]
@@ -38,8 +47,8 @@ export default function useApplicationData() {
       [id]: appointment
     };
     return axios
-    .put(`http://localhost:8001/api/appointments/${id}`, { interview })
-    .then((res) => {
+    .put(`/api/appointments/${id}`, { interview })
+    .then(() => {
       const days = updateSpots(state, id, appointments);
       setState({ ...state, appointments, days })
     })
@@ -55,22 +64,13 @@ export default function useApplicationData() {
       [id]: appointment
     };
     return axios
-    .put(`http://localhost:8001/api/appointments/${id}`)
-    .then((res) => {
+    .put(`/api/appointments/${id}`)
+    .then(() => {
       const days = updateSpots(state, id, appointments);
       setState({ ...state, appointments, days })
     })
   };
 
-  useEffect(() => {
-    Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-      })
-  }, []);
 
   return { state, setDay, bookInterview, cancelInterview }
 };
